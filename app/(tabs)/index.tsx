a91881c5a9ecf5029ae4a9147c9e078e428baa09
Adam Kapos <Nezz@users.noreply.github.com>
2025-09-14 11:18:23 +0300

    Add SWR

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -5,8 +5,18 @@ import { HelloWave } from '@/components/HelloWave';
 import ParallaxScrollView from '@/components/ParallaxScrollView';
 import { ThemedText } from '@/components/ThemedText';
 import { ThemedView } from '@/components/ThemedView';
+import useSWR from 'swr';
+import useSWRInfinite from 'swr/infinite';
 
 export default function HomeScreen() {
+  const { data, error, isLoading } = useSWR(`/api/user/test`, (url) => {});
+  const { mutate } = useSWRInfinite((index, previousData) => {
+    return `/api/user/test`;
+  }, (url) => {
+  });
+
+  console.log(data, mutate);
+  
   return (
     <ParallaxScrollView
       headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
